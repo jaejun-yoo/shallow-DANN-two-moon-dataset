5d7f4231340b8c15e6501691b9ca7cbaec4f784e
Jaejun Yoo <jaejun.yoo88@gmail.com>
2017-01-29 18:38:57 +0900

    Update gen_2moon.m
    correction for data input generation

--- gen_2moon.m
+++ gen_2moon.m
@@ -13,10 +13,10 @@ theta = linspace(0,pi,nb_sample);
 noise = rand(1,nb_sample)*0.6;
 semi_up = [(radius+noise).*cos(theta) + center1;(radius-1+noise).*sin(theta)-0.2];
 semi_down = [(radius+noise).*cos(-1*theta) + center2; (radius+noise).*sin(-1*theta)];
-figure;
-plot(semi_up(1,:),semi_up(2,:),'bo')
-hold on
-plot(semi_down(1,:),semi_down(2,:),'ro')
+% figure;
+% plot(semi_up(1,:),semi_up(2,:),'bo')
+% hold on
+% plot(semi_down(1,:),semi_down(2,:),'ro')
 
 x = [semi_up,semi_down]';
 y = [ones(length(semi_up),1);-1*ones(length(semi_down),1)];
@@ -31,15 +31,22 @@ theta = theta*pi/180;
 rotation = [  [cos(theta), sin(theta)]; [-sin(theta), cos(theta)] ];
 xt = xt * rotation;
 
-figure,
-plot(x(y==1,1),x(y==1,2),'bo')
+dataMean = mean(x);
+X = 2*bsxfun(@minus, x, dataMean);
+X_adapt = 2*bsxfun(@minus, xt, dataMean);
+theta = 35;
+theta = theta*pi/180;
+rotation = [  [cos(theta), sin(theta)]; [-sin(theta), cos(theta)] ];
+X_adapt = X_adapt*rotation;
+Y = ones(numel(y),1).*(y==1)+ 2*ones(numel(y),1).*(y==-1);
+
+figure, 
+plot(X(:,1),X(:,2),'bo')
 hold on 
-plot(x(y==-1,1),x(y==-1,2),'b+')
-plot(xt(yt==1,1),xt(yt==1,2),'ro')
-plot(xt(yt==-1,1),xt(yt==-1,2),'r+')
+plot(X_adapt(:,1),X_adapt(:,2),'ro')
 axis equal
 
-save('2Moons_v2','x','xt','y','yt');
+save('2Moons_v2','X','X_adapt','Y','x','xt','y','yt');
 
 
 
